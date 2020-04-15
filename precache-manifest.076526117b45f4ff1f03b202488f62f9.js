7e7ca13d00ca2429d6dbeb7080b1a82878c6c9c4
shangweihung <alannn1230@gmail.com>
2020-04-15 16:24:59 -0700

    Updates

---
+++ precache-manifest.076526117b45f4ff1f03b202488f62f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00d88ade13838647947b77d2cb2a1203",
+    "url": "/taiwan-gov-data/index.html"
+  },
+  {
+    "revision": "0912b25bf27c9f314af3",
+    "url": "/taiwan-gov-data/static/css/main.569a5c99.chunk.css"
+  },
+  {
+    "revision": "c08e8e339787a1c50eb9",
+    "url": "/taiwan-gov-data/static/js/2.b7141c8f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/taiwan-gov-data/static/js/2.b7141c8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0912b25bf27c9f314af3",
+    "url": "/taiwan-gov-data/static/js/main.f63e9faa.chunk.js"
+  },
+  {
+    "revision": "f505b455da273fe56a78",
+    "url": "/taiwan-gov-data/static/js/runtime-main.b6270f63.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/taiwan-gov-data/precache-manifest.076526117b45f4ff1f03b202488f62f9.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/taiwan-gov-data/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
